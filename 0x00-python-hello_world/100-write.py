e9b30ede539563f2729751d733ed49641a8404cf
Val-Ster <vster310@gmail.com>
2023-10-30 18:56:46 -0700

    error handling

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
-def main():
-    message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+
+message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
 # Write the message to stderr
 sys.stderr.write(message + '\n')
 # Exit with status code 1
