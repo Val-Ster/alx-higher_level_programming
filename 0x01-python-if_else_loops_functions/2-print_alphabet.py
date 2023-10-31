1fd63362480bff4bf9044362ffde0a1cf32e8ea2
Val-Ster <vster310@gmail.com>
2023-10-31 13:16:00 -0700

    ASCII

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ascii_value in range(97, 123): # ASCII 4 lowercase 'a' - 'z' re 97 - 122
+for ascii_value in range(97, 123):  # ASCII 4 lowercase 'a' - 'z' re 97 - 122
     char = chr(ascii_value)
 print(char, end=' ')
