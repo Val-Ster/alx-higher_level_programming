65e24cfe4bfaa8db79a6c9d20e9248ea2ad18062
Val-Ster <vster310@gmail.com>
2023-10-30 18:49:27 -0700

    scripts and bytecode

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * check_cycle - Check if a singly linked list has a cycle.
+ * @list: Pointer to the head of the linked list.
+ *
+ * Description: This function checks whether a given singly linked list
+ * has a cycle or not. It uses the Floyd's Cycle Detection Algorithm.
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
