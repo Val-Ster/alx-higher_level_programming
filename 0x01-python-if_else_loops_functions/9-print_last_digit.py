f4cc8957326a8baaeaf37bf2a4a3596e74e08ee1
Val-Ster <vster310@gmail.com>
2023-10-31 16:35:09 -0700

    sum of no.s

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    # Ensure the number is positive for the last digit calculation
-    if number < 0:
-        number = -number
+    num = number
+    if num < 0:
+        num *= -1
+    last = num % 10
 
-    # Get the last digit using modulo operator
-    last_digit = number % 10
-
-    # Print the last digit
-    print(last_digit, end='')
-
-    # Return the last digit as an integer
-    return last_digit
-
-# Test cases
- if __name__ == "__main__":
-    print_last_digit(98)
-    print_last_digit(0)
-    r = print_last_digit(-1024)
-    print(r)
+    while num > 9:
+        last = num % 10
+        num = last
+    print(last, end="")
+    return (last)
