95cc7a8e291d2d4b0827872fe19c356b4806972f
Val-Ster <vster310@gmail.com>
2023-10-31 15:31:44 -0700

    ascii

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
-exempted_letters = ['e', 'q']
-output = ""
-
-for ascii_value in range(97, 123):
-    char = chr(ascii_value)
-    if char not in exempted_letters:
-        output += char
-
-print(f"{output}", end=' ')
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
