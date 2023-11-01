842057582ff0d89639c56acd969acb46340a6522
Val-Ster <vster310@gmail.com>
2023-10-31 17:24:06 -0700

    print_tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
+for i in range(ord('z'), ord('A') - 1, -1):
     if i % 2 == 0:
-        letter = chr(i).upper()
-    else:
         letter = chr(i)
+    else:
+        letter = chr(i).upper()
     print("{}".format(letter), end='')
