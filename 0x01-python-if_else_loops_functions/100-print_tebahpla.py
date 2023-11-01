adca1ec35fd2054fd7dafd1343d552f282ef324e
Val-Ster <vster310@gmail.com>
2023-10-31 17:06:43 -0700

    print_tebahpla

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        letter = chr(i).upper()
+    else:
+        letter = chr(i)
+    print("{}".format(letter), end='')
+
+print()
