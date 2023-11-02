b59dadfd53b7e6d00c44131fa5063a32a9603296
Val-Ster <vster310@gmail.com>
2023-11-02 15:57:02 -0700

    correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 # Related third-party imports
-from magic_calculation_102 import add, sub
-
-
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
-
+from magic_calculation_102 import add, sub
 
 if a < b:
-    result = add(a, b)
+    c = add(a, b)
 for i in range(4, 6):
-    result = add(result, i)
-return result
-else:
-    return sub(a, b)
+    c = add(c, i)
+return c
+return sub(a, b)
