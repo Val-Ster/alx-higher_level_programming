33a93f7dca6ca07322cada1c978b8fbe67053051
Val-Ster <vster310@gmail.com>
2023-11-02 16:38:22 -0700

    correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,13 +6,13 @@ def magic_calculation(a, b):
 
 
 if a < b:
-    result = add(a, b)
+    c = add(a, b)
 else:
     return sub(a, b)
 
 
 for i in range(4, 6):
-    result = add(result, i)
+    c = add(c, i)
 
 
-return result
+return c
