5645c994b257bdb94bea323524db40de690482a1
Val-Ster <vster310@gmail.com>
2023-11-06 07:55:46 -0800

    return position correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,14 +3,13 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
 
-if a < b:
-    c = add(a, b)
-else:
-    return sub(a, b)
+    if a < b:
+        c = add(a, b)
+    else:
+        return sub(a, b)
 
 
-for i in range(4, 6):
-    c = add(c, i)
+    for i in range(4, 6):
+        c = add(c, i)
 
-
-return c
+    return c
