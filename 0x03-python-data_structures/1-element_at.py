4c9ef5a78718fb0b3a0ff4df2e83016fff99aec7
Val-Ster <vster310@gmail.com>
2023-11-06 06:37:20 -0800

    element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,15 +5,13 @@ def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         # it alows bothi.e if i use just d if<0 nd if i use d if nd d or >=
         return None
-# Initialize a variable to keep track of the current index
-
-
-current_idx = 0
-# Iterate through the list
-for element in my_list:
-    # If the current index matches the desired index, return the element
-    if current_idx == idx:
-        return element
-current_idx += 1
-# If idx is out of range, return None
-return none
+    # Initialize a variable to keep track of the current index
+    current_idx = 0
+    # Iterate through the list
+    for element in my_list:
+        # If the current index matches the desired index, return the element
+        if current_idx == idx:
+            return element
+        current_idx += 1
+        # If idx is out of range, return None
+        return none
