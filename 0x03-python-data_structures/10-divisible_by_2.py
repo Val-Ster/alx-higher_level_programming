dbc1f45057ce0ff7b772f0a36d581c0ad6693dcf
Val-Ster <vster310@gmail.com>
2023-11-06 07:21:35 -0800

    more corrections

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,6 @@ def divisible_by_2(my_list=[]):
     # Initialize an empty list to store the results
     result = []
     # Iterate through the original list
-
-
     for num in my_list:
         # Check if the integer is divisible by 2
         is_divisible = num % 2 == 0
