dc2e394150281d609b292a24740423eeb3ae3f32
Val-Ster <vster310@gmail.com>
2024-01-16 18:09:39 -0800

    MYSQL

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,5 +1,3 @@
 -- Delete Database hbtn_0c_0
 -- If the database doesn’t exist, your script should not fail
-```
 DROP DATABASE IF NOT EXISTS hbtn_0c_0;
-```
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,6 @@
 -- Create a Table called first_name
 -- Description: id, name
-```
 CREATE TABLE IF NOT EXISTS first_name (
 	id INT,
 	name VARCHAR(256)
 );
-```
