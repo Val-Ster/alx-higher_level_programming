8a3b6f4f6a1056a524ff904fdb5511765fc3ad2c
Val-Ster <vster310@gmail.com>
2024-04-25 14:40:28 +0000

    Printing multiple lines using arrays and loops

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 // Array of strings
-let statements = [
-    "C is fun",
-    "Python is cool",
-    "JavaScript is amazing"
+const statements = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 // Loop through the array and print each statement
 for (let i = 0; i < statements.length; i++) {
-    console.log(statements[i]);
+  console.log(statements[i]);
 }
