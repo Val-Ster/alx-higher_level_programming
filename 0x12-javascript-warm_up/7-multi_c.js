59594c5bc6133b1872334b2fb4bc7ba531d25b51
Val-Ster <vster310@gmail.com>
2024-04-25 16:35:42 +0000

    a script that prints 'C is fun' x times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
 // Get the number of occurrences from the command-line argument
-let numOccurrences = parseInt(process.argv[2]);
+const numOccurrences = parseInt(process.argv[2]);
 
 // Check if the argument is a valid number
 if (!isNaN(numOccurrences)) {
-    // Print "C is fun" the specified number of times using a loop
-    for (let i = 0; i < numOccurrences; i++) {
-        console.log("C is fun");
-    }
+  // Print "C is fun" the specified number of times using a loop
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log('C is fun');
+  }
 } else {
-    // Print an error message if the argument is not a valid number
-    console.log("Missing number of occurrences");
+  // Print an error message if the argument is not a valid number
+  console.log('Missing number of occurrences');
 }
