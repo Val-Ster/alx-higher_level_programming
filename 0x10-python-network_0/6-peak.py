172ea47bf9fb9420ecaebe5bc47b04d74a5d6c0c
Val-Ster <Vster310@gmail.com>
2024-05-09 13:34:59 -0700

    Finding the peak in a list of integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -29,7 +29,8 @@ def find_peak(list_of_integers):
         return list_of_integers[length - 1]
 
     for i in range(1, length - 1):
-        if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
+        if (list_of_integers[i] >= list_of_integers[i - 1] and
+                list_of_integers[i] >= list_of_integers[i + 1]):
             return list_of_integers[i]
 
     return None
