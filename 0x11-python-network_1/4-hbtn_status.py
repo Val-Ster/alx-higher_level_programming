b98741bee3f8ee031c247fc13bfa47b3a66dc62f
Val-Ster <vster310@gmail.com>
2024-05-17 13:53:15 -0700

    Python Networking

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status
+using the requests package.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
