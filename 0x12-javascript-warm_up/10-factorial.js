4f38158719508b1a322fa5f4c425746d067a943d
Val-Ster <vster310@gmail.com>
2024-05-17 06:12:00 -0700

    Javascripting

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Define the factorial function
+function factorial(n) {
+	if (isNaN(n) || n <= 1) {
+		return 1;
+	}
+	return n * factorial(n - 1);
+}
+
+// Get the first argument from the command line and convert it to an integer
+const arg = parseInt(process.argv[2]);
+
+// Print the result of the factorial computation
+console.log(factorial(arg));
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// Define the add function
+function add(a, b) {
+	return a + b;
+}
+
+// Get the first and second arguments from the command line
+const firstArg = parseInt(process.argv[2]);
+const secondArg = parseInt(process.argv[3]);
+
+// Check if both arguments are valid numbers
+if (isNaN(firstArg) || isNaN(secondArg)) {
+	console.log('NaN');
+} else {
+	// Print the result of the addition
+	console.log(add(firstArg, secondArg));
+}
